293d9b8229c7a05e9af9ee5edf0d8a02c06c438e
Nicolas Ettlin <nicolas@convex.dev>
2025-04-10 15:02:20 -0700

    Move NotFound

--- app/components/ExistingChat.client.tsx
+++ app/components/ExistingChat.client.tsx
@@ -10,7 +10,6 @@ import { ContainerBootState, useContainerBootState } from '~/lib/stores/containe
 import { useReloadMessages } from '~/lib/stores/startup/reloadMessages';
 import { useSplines } from '~/lib/splines';
 import { UserProvider } from '~/components/UserProvider';
-import { NotFound } from './NotFound';
 
 export function ExistingChat({ chatId }: { chatId: string }) {
   // Fill in the chatID store from props early in app initialization. If this
@@ -96,5 +95,24 @@ function ExistingChatWrapper({ chatId }: { chatId: string }) {
         />
       )}
     </>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="flex h-full justify-center items-center text-center flex-col p-8">
+      <h1 className="text-4xl font-bold mb-4 font-display font-600 tracking-tight text-bolt-elements-textPrimary">
+        Not found
+      </h1>
+      <p className="text-bolt-elements-textSecondary mb-4 text-balance">
+        The Chef project you’re looking for can’t be found. Maybe it was deleted or created with another account?
+      </p>
+      <a
+        href="/"
+        className="inline-flex gap-2 items-center bg-bolt-elements-button-primary-background hover:bg-bolt-elements-button-primary-backgroundHover text-bolt-elements-button-primary-text hover:text-bolt-elements-button-primary-textHover rounded-lg px-4 py-2 transition-colors"
+      >
+        <span className="text-sm font-medium">Return home</span>
+      </a>
+    </div>
   );
 }
