3faed7d3f923975ed9aa795951642d1106b816bc
Nicolas Ettlin <nicolas@convex.dev>
2025-04-14 17:28:52 -0700

    Use a single Toaster component

---
+++ app/components/ui/Toaster.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-restricted-imports
+import { Toaster as SonnerToaster } from 'sonner';
+
+export function Toaster() {
+  return <SonnerToaster position="bottom-right" closeButton richColors />;
+}
