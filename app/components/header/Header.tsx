deb275317ca339ff397409ef0cdabf38e44e97b6
Indy Khare <ikhare@convex.dev>
2025-04-16 08:34:26 -0700

    Remove feedback button add download button to header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,10 +4,10 @@ import { chatStore } from '~/lib/stores/chatId';
 import { HeaderActionButtons } from './HeaderActionButtons.client';
 import { ChatDescription } from '~/components/header/ChatDescription.client';
 import { DeployButton } from './DeployButton';
-import { FeedbackButton } from './FeedbackButton';
 import { ShareButton } from './ShareButton';
 import { useConvexSessionIdOrNullOrLoading } from '~/lib/stores/sessionId';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { DownloadButton } from './DownloadButton';
 
 export function Header({ hideSidebarIcon = false }: { hideSidebarIcon?: boolean }) {
   const chat = useStore(chatStore);
@@ -50,7 +50,7 @@ export function Header({ hideSidebarIcon = false }: { hideSidebarIcon?: boolean 
           <ClientOnly>
             {() => (
               <div className="flex flex-wrap items-center gap-2">
-                <FeedbackButton />
+                <DownloadButton />
                 <ShareButton />
                 <DeployButton />
                 <div className="mr-1">
