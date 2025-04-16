b77dbece7af78dac869a4e0148f4c6049cf78332
Nicolas Ettlin <nicolas@convex.dev>
2025-04-16 15:03:17 -0700

    Memoize SendButton

--- app/components/chat/SendButton.client.tsx
+++ app/components/chat/SendButton.client.tsx
@@ -2,6 +2,7 @@ import { AnimatePresence, cubicBezier, motion } from 'framer-motion';
 import { ArrowRightIcon, StopIcon } from '@radix-ui/react-icons';
 import { classNames } from '~/utils/classNames';
 import { Button, buttonClasses } from '@ui/Button';
+import React from 'react';
 
 interface SendButtonProps {
   show: boolean;
@@ -15,7 +16,7 @@ const customEasingFn = cubicBezier(0.4, 0, 0.2, 1);
 
 const MotionButton = motion.create(Button);
 
-export const SendButton = ({ show, isStreaming, disabled, onClick, tip }: SendButtonProps) => {
+export const SendButton = React.memo(({ show, isStreaming, disabled, onClick, tip }: SendButtonProps) => {
   return (
     <AnimatePresence>
       {show ? (
@@ -43,4 +44,4 @@ export const SendButton = ({ show, isStreaming, disabled, onClick, tip }: SendBu
       ) : null}
     </AnimatePresence>
   );
-};
+});
