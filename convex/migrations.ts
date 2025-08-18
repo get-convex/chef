e1f72957d6b258da0cb80e96bcaa4fba2a2d89df
Ari <ari@convex.dev>
2025-08-18 11:50:52 -0700

    fix

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,6 +1,6 @@
 import { Migrations } from "@convex-dev/migrations";
 import { components, internal } from "./_generated/api.js";
-import { Doc } from "./_generated/dataModel.js";
+import type { Doc } from "./_generated/dataModel.js";
 
 export const migrations = new Migrations(components.migrations);
 export const run = migrations.runner();
